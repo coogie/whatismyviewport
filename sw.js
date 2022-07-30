995dc10d1d479fef1fa44cda7103f32c2dbd5066
Stephen Coogan <coogie@users.noreply.github.com>
2022-07-30 04:42:37 +0100

    feat: add dimension for network status

--- sw.js
+++ sw.js
@@ -4,7 +4,11 @@ workbox.setConfig({ modulePathPrefix });
 workbox.loadModule('workbox-google-analytics')
 workbox.loadModule('workbox-precaching')
 
-workbox.googleAnalytics.initialize();
+workbox.googleAnalytics.initialize({
+    parameterOverrides: {
+      cd1: 'offline',
+    },
+});
 workbox.precaching.precacheAndRoute([
     { url: '/index.html',                   revision: '1' },
     { url: '/404.html',                     revision: '1' },
